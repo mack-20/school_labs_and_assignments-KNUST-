1cd05e871c782894f59856e2e4183d25a3530c30
Mackleode Ben-Boulo <mackleodebenboulo@gmail.com>
2023-02-12 16:10:26 +0000

    First KNUST LABS(OOP) commit.

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int sum(int);
+double cmToFeet(double);
+void generateFibSequence(int limit);
+
+int main(){
+	int option = 0;
+	while(true){
+		int N = 0;
+		double height = 0;
+		int limit = 0;
+		cout << "\nWhat would you like to do?"<<endl;
+		cout << "1.Calculate sum of first N natural numbers.\n2.Convert height in cm to feet.\n3.Generate Fibonacci sequence to a given number\n4.Quit\n";
+		cout << ">> ";
+		cin >> option;
+
+		switch (option){
+		case 1:
+			cout << "Enter a value for N: ";
+			cin >> N;
+			cout << "\nThe sum of the first "<<N<<" natural numbers is "<<sum(N)<<"."<<endl;
+			break;
+		case 2:
+			cout << "Enter height in cm: ";
+			cin >> height;
+			cout << setprecision(3) << "Height in Feet: "<<cmToFeet(height)<<"ft"<<endl;
+			break;
+		case 3:
+			cout << "Enter number limit: ";
+			cin >> limit;
+			generateFibSequence(limit);
+			break;
+		default:
+			cout << "Invalid option. Try again?"<<endl;
+			break;
+		case 4:
+			exit(0);
+		}
+	}
+	return 0;
+}
+
+
+//Using recursion, cos why not (:
+int sum(int n){
+	if(n == 1)
+		return 1;
+	else
+		return (n + sum( n - 1));
+}
+
+//Using inline since function is just a one line code implementation
+inline double cmToFeet(double heightInCm){
+	return (heightInCm * 0.0328084);
+}
+
+void generateFibSequence(int limit){
+	int currentFibNum = 0;
+	int tempFibNum = 1;
+	int prevFibNum = 0;
+	cout << "[ ";
+	for(; limit > currentFibNum;){
+		cout <<currentFibNum << "  ";
+		if(currentFibNum == 0)
+			currentFibNum = tempFibNum;
+		else{
+			tempFibNum = currentFibNum;
+			currentFibNum += prevFibNum;
+			prevFibNum = tempFibNum;
+		}
+	}
+	cout << " ]";
+}
